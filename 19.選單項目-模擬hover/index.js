98bd4b784b65121cfc9927d08f73d5c4a3d9f454
josie-shie <joe003218@gmail.com>
2021-04-15 16:03:44 +0800

    4/15 noon

--- 19.選單項目-模擬hover/index.js
+++ 19.選單項目-模擬hover/index.js
@@ -2,8 +2,8 @@ const menuItems = document.getElementsByClassName('menu-item')
 
 for (let i = 0; i < menuItems.length; i++) {
   menuItems[i].addEventListener('mouseenter', function (event) {
-    event.target.childNodes[1].classList.add('enter')
-
+    event.target.childNodes[1].classList.add('enter',)
+    //在class屬性裡加上enter
     console.log('mouseenter', event.target.innerText)
   })
 
