2e542caaa0fa3bc84063f1bdeb3d49a1d747aba1
josie-shie <joe003218@gmail.com>
2021-04-16 16:43:04 +0800

    4/16

---
+++ 24.考試內容/23.飲料店表單/介面/index.js
@@ -0,0 +1,18 @@
+const teaGreen = document.querySelector('#tea-green')
+const teaRed = document.querySelector('#tea-red')
+const sugarText = document.querySelector('#sugar-text')
+const sugarNormal = document.querySelector('#sugar-normal')
+const sugarHalf = document.querySelector('#sugar-half')
+const iceNormal = document.querySelector('#ice-normal')
+const iceHalf = document.querySelector('#ice-half')
+const countText = document.querySelector('#count-text')
+const addOne = document.querySelector('#add-one')
+const subOne = document.querySelector('#sub-one')
+const addOrder = document.querySelector('#add-order')
+const resetOrder = document.querySelector('#reset-order')
+const dataTable = document.querySelector('#dataTable')
+
+
+teaGreen.addEventListener('click',function(){
+    dataTable.innerHTML=``
+})
